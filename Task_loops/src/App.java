4eee0e5e5d89daf3c8d36f827ede05bc29cdd9ea
AlKousOthman <alkousothman@gmail.com>
2024-01-03 20:58:52 +0300

    Loops task

---
+++ Task_loops/src/App.java
@@ -0,0 +1,35 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //System.out.println("Hello, World!");
+
+
+        // for (int i = 5; i <= 6; i++){
+        //    System.out.println(i);
+        // }
+
+            // int s = 1;
+            // while ( s <= 10){
+            //     System.out.println(s);
+            //     s++;
+            // }
+
+            String[] Days ={"Sun.", "Mon.", "Tue.", "Wed.", "Thur", "Fri.", "Sat."};
+
+            for( int i = 0; i < Days.length; i++ ){
+
+                if(i < 5){
+
+                System.out.println(Days[i]);
+                }
+                else{
+                    break;
+                }
+
+
+            }
+
+
+
+        
+    }
+}
